a12e45a739b78d934b3d755a0f64813716387f62
zoey <khionewinterderg@gmail.com>
2023-06-01 12:03:33 -0700

    ./dothegit

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the sizes
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char charType;
+	int intType;
+	long longType;
+	long long longLongType;
+	float floatType;
+
+	printf("Size of a char: $zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: $zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: $zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: $zu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: $zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+
+}
