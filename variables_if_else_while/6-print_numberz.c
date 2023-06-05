d2b10c008faa0672b42e0a1a42f2e2cad086d347
zoey <khionewinterderg@gmail.com>
2023-06-05 14:36:14 -0700

    sdddgdsagdsag

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints every char in the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	printf("\n");
+	return (0);
+}
