c374c5f3b9eb7fad78e4c7ebf6fa69a8a08886e7
zoey <khionewinterderg@gmail.com>
2023-06-05 13:47:52 -0700

    ewytyeyuqiweyt

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - prinst nummbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
